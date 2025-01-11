37c2531bf7ec952b1f5e4e1938af138b098968dd
Lucky <103184937+LuckySikolia@users.noreply.github.com>
2025-01-12 02:08:05 +0300

    Initial commit

---
+++ Assets/Scripts/ObjectPooling.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooling : MonoBehaviour
+{
+    //this script handles the pooling and spawning of objects in the game
+
+
+}
